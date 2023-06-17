8fafc3c6f088b02cae4cd41ab4fdaf72629b62dd
sms <creepercannyon@gmail.com>
2023-06-17 11:54:12 -0700

    Attempting to make the object select better

--- scripts/editor/object_select.gd
+++ scripts/editor/object_select.gd
@@ -2,21 +2,14 @@ extends Control
 
 signal selected(node: PackedScene)
 
-func add_object(obj: Dictionary):
-# Assume obj contains the following:
-# prefab: PackedScene
-# name: String
-
-# Assume instantiated prefab contains the following:
-# position: Vector2
-# width: int
-# height: int
+func add_object(obj: Dictionary): # TODO: Figure out a better way
 	var prefabScene: PackedScene = obj.get("prefab")
 	var name: String = obj.get("name")
 	var templateScene = preload("res://prefabs/editor/object_template.tscn")
-	var template = templateScene.instantiate()
+	var template = templateScene.instantiate() as Control
 	var prefab = prefabScene.instantiate()
-	prefab.prepare_as_sample(Vector2(64, 64))
+	print(template.custom_minimum_size)
+	prefab.prepare_as_sample(Vector2(template.custom_minimum_size.x, template.custom_minimum_size.x))
 	template.add_child(prefab)
 	template.get_node("Label").text = name
 	template.pressed.connect(func(): 
